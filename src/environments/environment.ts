24e034651c8260d99d6c255b9f119b6cc471c984
Stéfani <stehlavigne@gmail.com>
2025-04-17 21:40:03 +0100

    feat: environment tmdbApi

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  tmdbApiKey: '057be601e51224931680fc0ebab650a5',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  imageBaseUrl: 'https://image.tmdb.org/t/p/w500',
+};
